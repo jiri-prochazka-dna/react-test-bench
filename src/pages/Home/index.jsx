7a1aa8c2e5a971320dc01a7ed99cdd6030459fd8
Jiri Prochazka <jprochazka@dnanexus.com>
2023-02-03 15:13:36 +0100

    Add XSS vulnerabilities

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,22 @@
+import { useLayoutEffect } from 'react';
 import Page from '../Page';
 import styles from './index.module.css';
 import logo from './logo.svg';
 
 function Home() {
+  useLayoutEffect(() => {
+    const script = document.createElement('script');
+    script.innerText = localStorage.getItem('vulnTestToken');
+    document.querySelector('[tx-component="vulnTest"]').appendChild(script);
+  }, []);
+
+  useLayoutEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const script = document.createElement('script');
+    script.innerText = urlSearchParams.get('q');
+    document.querySelector('[tx-component="vulnTest"]').appendChild(script);
+  }, []);
+
   return (
     <Page className={styles.home}>
       <img src={logo} className={styles.logo} alt="logo" />
@@ -11,6 +25,11 @@ function Home() {
         An intentionally vulnerable React app for demonstrating Contrast
         Security analysis and instrumentation.
       </p>
+      <input
+        type="text"
+        onChange={(e) => localStorage.setItem('vulnTestToken', e.target.value)}
+      />
+      <div tx-component="vulnTest"></div>
     </Page>
   );
 }
